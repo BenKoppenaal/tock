ba2b45942fa21848609a0e1a6e7495085bf786f0
Ben Koppenaal <ben@koppenaal.me>
2026-09-09 22:11:38 -0600

    tweak task list search and some positioning

--- ui/tasklist.go
+++ ui/tasklist.go
@@ -1,6 +1,7 @@
 package ui
 
 import (
+	"strings"
 	"tock/model"
 
 	"github.com/charmbracelet/bubbles/list"
@@ -18,8 +19,9 @@ func (t taskItem) FilterValue() string { return t.Name }
 type startEntryMsg struct{ task model.Task }
 
 type TaskListModel struct {
-	list   list.Model
-	search textinput.Model
+	list     list.Model
+	search   textinput.Model
+	allTasks []model.Task
 }
 
 func NewTaskListModel(tasks []model.Task) TaskListModel {
@@ -32,16 +34,29 @@ func NewTaskListModel(tasks []model.Task) TaskListModel {
 	l.SetShowTitle(false)
 	l.SetShowHelp(false)
 	l.SetFilteringEnabled(false)
+	l.SetStatusBarItemName("task", "tasks")
 
 	search := textinput.New()
-	search.Placeholder = "/ to search tasks..."
+	search.Placeholder = "Search tasks..."
 	search.CharLimit = 100
 
-	return TaskListModel{list: l, search: search}
+	return TaskListModel{list: l, search: search, allTasks: tasks}
 }
 
 func (m TaskListModel) setSize(w, h int) TaskListModel {
 	m.list.SetSize(w, h-4)
+	return m
+}
+
+func (m TaskListModel) applyFilter() TaskListModel {
+	q := strings.ToLower(m.search.Value())
+	items := make([]list.Item, 0, len(m.allTasks))
+	for _, t := range m.allTasks {
+		if q == "" || strings.Contains(strings.ToLower(t.Name), q) {
+			items = append(items, taskItem{t})
+		}
+	}
+	m.list.SetItems(items)
 	return m
 }
 
@@ -59,6 +74,7 @@ func (m TaskListModel) Update(msg tea.Msg) (TaskListModel, tea.Cmd) {
 			if m.search.Focused() {
 				m.search.Blur()
 				m.search.SetValue("")
+				m = m.applyFilter()
 				return m, nil
 			}
 		case "enter":
@@ -76,6 +92,7 @@ func (m TaskListModel) Update(msg tea.Msg) (TaskListModel, tea.Cmd) {
 		var cmd tea.Cmd
 		m.search, cmd = m.search.Update(msg)
 		cmds = append(cmds, cmd)
+		m = m.applyFilter()
 	} else {
 		var cmd tea.Cmd
 		m.list, cmd = m.list.Update(msg)
@@ -87,8 +104,14 @@ func (m TaskListModel) Update(msg tea.Msg) (TaskListModel, tea.Cmd) {
 
 func (m TaskListModel) View() string {
 	help := helpStyle.Render("n: new task  / search  enter: start/stop  q: quit")
+	if m.search.Focused() {
+		return lipgloss.JoinVertical(lipgloss.Left,
+			lipgloss.NewStyle().PaddingLeft(2).Render(m.search.View()),
+			m.list.View(),
+			help,
+		)
+	}
 	return lipgloss.JoinVertical(lipgloss.Left,
-		m.search.View(),
 		m.list.View(),
 		help,
 	)
